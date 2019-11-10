f6536992d11acf1b771fd915126d2d20b1ff452c
connor worthen <connorw220@protonmail.com>
2019-11-10 19:55:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+name_hash = {
+  :apples => 2, :oranges => 1, :grapes => 3
+}
 
 def key_for_min_value(name_hash)
-
+  name_hash.key(name_hash.values.min)
 end
