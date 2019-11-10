60bf086d5dbd4193a6b7fac85387a35a97ca65b7
connor worthen <connorw220@protonmail.com>
2019-11-10 20:05:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,17 @@ name_hash = {
 }
 
 def key_for_min_value(name_hash)
-  name_hash.select { |k,v| k < v }
+   min_key = nil
+   min_value = nil
+   name_hash.each do |key, value|
+      #1st iteration: the min_value is nil so the first value is the min_value
+      #2nd: now the min_value is not nil, check for the next condition
+      #without "min_value == nil", we can't check for the smallest value
+      #because it can't meet the condition "value < min_value"
+      if min_value == nil || value < min_value
+         min_value = value
+         min_key = key
+      end
+   end
+   min_key
 end
